3b6660948a0f052bb07a32913bd8b33c9b29a365
parkjunhee0510 <junhee5143@naver.com>
2023-10-25 12:18:57 +0900

    Version-1.2

---
+++ accounts/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "kakaoid",
+            "name",
+            "profile",
+            "type",
+            "created_at",
+            "is_active",
+            "is_staff",
+        ]
+        # fields = "__all__"
