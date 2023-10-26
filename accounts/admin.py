553173cdd5d66e56edfb49a58033cd360ceb400d
HOTARU <dusckd4948@naver.com>
2023-10-26 23:41:53 +0900

    어드민 페이지 사진 업데이트함

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,16 @@
+from django.utils.html import format_html
 from django.contrib import admin
 from .models import User
 
-# Register your models here.
 
+# Register your models here.
 class UserAdmin(admin.ModelAdmin):
+    list_display = ['kakaoid','name','get_image']
+    list_display_links = ['kakaoid']
     list_per_page = 10
 
-
+    def get_image(self, obj):
+        return format_html('<img src="{}" width="50" height="50" />', obj.profile.url)
+    get_image.short_description = 'profile'
+    
 admin.site.register(User,UserAdmin)
