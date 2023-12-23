d7238950a7a4cae4713540582f1ee4f9cef68039
YyyyyyCH <128468098+OHTARU@users.noreply.github.com>
2023-12-23 13:04:49 +0900

    urls.py 업데이트

--- post/urls.py
+++ post/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('postview/', PostListView.as_view(), name='post_list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('mypost/', GetMyPostView.as_view(), name='post_mypost'),
-    path('post/<int:pk>/create/', PostCreateView.as_view(), name='post_create'),
+    path('post/create/', PostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
