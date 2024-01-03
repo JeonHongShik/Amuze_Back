8e8e920816e2ac7cb1b83e81be3affcb6c1be7ed
ohtaru <dusckd4948@naver.com>
2024-01-03 16:41:03 +0900

    deadline,datetime change datetime > string

--- post/serializers.py
+++ post/serializers.py
@@ -10,8 +10,8 @@ class PostSerializer(serializers.ModelSerializer):
     # otherimages3 = serializers.ImageField(max_length=None, use_url=True)
     # otherimages4 = serializers.ImageField(max_length=None, use_url=True)
     author = serializers.SerializerMethodField('get_author_name')
-    deadline = serializers.SerializerMethodField()
-    datetime = serializers.SerializerMethodField()
+    # deadline = serializers.SerializerMethodField()
+    # datetime = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
@@ -20,22 +20,30 @@ class PostSerializer(serializers.ModelSerializer):
     def get_author_name(self, obj):
         return obj.author.displayName
 
-    def get_deadline(self, obj):
-        if obj.deadline:
-            deadline = datetime.strptime(obj.deadline, '%Y-%m-%d %I:%M %p')
-            return deadline.strftime('%Y-%m-%d %I:%M %p')
-        else:
-            return None
+    # def get_deadline(self, obj):
+    #     if obj.deadline:
+    #         if isinstance(obj.deadline, str):
+    #             deadline = datetime.strptime(obj.deadline, '%Y-%m-%d %I:%M %p')
+    #         else:
+    #             deadline = obj.deadline
+    #         return deadline.strftime('%Y-%m-%d %I:%M %p')
+    #     else:
+    #         return None
 
-    def get_datetime(self, obj):
-        if obj.datetime:
-            datetime_obj = datetime.strptime(obj.datetime, '%Y-%m-%d %I:%M %p')
-            return datetime_obj.strftime('%Y-%m-%d %I:%M %p')
-        else:
-            return None
-    
-    def to_internal_value(self, data):
-        internal_value = super().to_internal_value(data)
-        internal_value['deadline'] = datetime.strptime(data.get('deadline'), '%Y-%m-%d %I:%M %p')
-        internal_value['datetime'] = datetime.strptime(data.get('datetime'), '%Y-%m-%d %I:%M %p')
-        return internal_value
+    # def get_datetime(self, obj):
+    #     if obj.datetime:
+    #         if isinstance(obj.datetime, str):
+    #             datetime_obj = datetime.strptime(obj.datetime, '%Y-%m-%d %I:%M %p')
+    #         else:
+    #             datetime_obj = obj.datetime
+    #         return datetime_obj.strftime('%Y-%m-%d %I:%M %p')
+    #     else:
+    #         return None
+        
+    # def to_internal_value(self, data):
+    #     internal_value = super().to_internal_value(data)
+    #     if isinstance(data.get('deadline'), str):
+    #         internal_value['deadline'] = datetime.strptime(data.get('deadline'), '%Y-%m-%d %I:%M %p')
+    #     if isinstance(data.get('datetime'), str):
+    #         internal_value['datetime'] = datetime.strptime(data.get('datetime'), '%Y-%m-%d %I:%M %p')
+    #     return internal_value
