257f056791f0274760a0622f057812b5042102e2
YyyyyyCH <128468098+OHTARU@users.noreply.github.com>
2024-01-05 02:58:16 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -105,21 +105,10 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "amuze",
-        "USER": "amuzemaster",
-        "PASSWORD": "Amuzemasterkey1234@",
-        "HOST": "3.39.21.42",
-        "PORT": "3306",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
