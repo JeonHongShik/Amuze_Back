c552f90da4473367c0b991352c6cf6850f67b4ea
ohtaru <dusckd4948@naver.com>
2024-01-16 15:08:34 +0900

    change fcm_token view

--- message_token/views.py
+++ message_token/views.py
@@ -6,23 +6,23 @@ from django.http import HttpResponse
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-def send_to_firebase_cloud_messaging(request, uid):  
-    user = User.objects.get(uid=uid)
-    registration_token = user.messagingToken
-    print('Registration token:', registration_token)
+# def send_to_firebase_cloud_messaging(request, uid):  
+#     user = User.objects.get(uid=uid)
+#     registration_token = user.messagingToken
+#     print('Registration token:', registration_token)
 
-    message = messaging.Message(
-    notification=messaging.Notification(
-        title='안녕하세요 타이틀 입니다',
-        body='안녕하세요 메세지 입니다',
-    ),
-    token=registration_token,
-    )
+#     message = messaging.Message(
+#     notification=messaging.Notification(
+#         title='안녕하세요 타이틀 입니다',
+#         body='안녕하세요 메세지 입니다',
+#     ),
+#     token=registration_token,
+#     )
 
-    response = messaging.send(message)
-    print('Successfully sent message:', response)
+#     response = messaging.send(message)
+#     print('Successfully sent message:', response)
 
-    return HttpResponse("Message sent successfully")
+#     return HttpResponse("Message sent successfully")
 
 
 @receiver(post_save, sender=Comment)
