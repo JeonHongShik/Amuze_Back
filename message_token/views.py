a869259b4436f92596f9bb3d1c742827011719dd
ohtaru <dusckd4948@naver.com>
2024-01-26 18:05:15 +0900

    message_token patch

--- message_token/views.py
+++ message_token/views.py
@@ -13,10 +13,19 @@ from rest_framework.views import APIView
 from rest_framework import status
 from django.db import transaction
 
+
 @receiver(post_save, sender=Comment)
 def send_to_firebase_cloud_messaging(sender, instance, created, **kwargs):
     if created:
-        user = instance.author
+        # 게시글 작성자를 가져옵니다.
+        user = instance.board.author
+        # 댓글 작성자를 가져옵니다.
+        comment_author = instance.author
+        
+        # 게시글 작성자와 댓글 작성자가 동일한 경우, 알림을 보내지 않습니다.
+        if user == comment_author:
+            return
+
         registration_token = user.messagingToken
         print('Registration token:', registration_token)
 
@@ -32,13 +41,39 @@ def send_to_firebase_cloud_messaging(sender, instance, created, **kwargs):
         response = messaging.send(message)
         print('Successfully sent message:', response)
 
-        # 알림을 생성합니다.
         Notification.objects.create(
             uid=user,
             title=instance.board.title,
             content=f'{instance.board.title} 새로운 댓글이 달렸어요!',
-            board_id=instance.board.id,
+            board_id=instance.board,
         )
+
+# @receiver(post_save, sender=Comment)
+# def send_to_firebase_cloud_messaging(sender, instance, created, **kwargs):
+#     if created:
+#         user = instance.board.author
+#         registration_token = user.messagingToken
+#         print('Registration token:', registration_token)
+
+#         message = messaging.Message(
+#             notification=messaging.Notification(
+#                 title='새로운 댓글 알림',
+#                 body=f'{instance.board.title}\n 새로운 댓글이 달렸어요!',
+#             ),
+#             data={'board_id':str(instance.board.id)},
+#             token=registration_token,
+#         )
+
+#         response = messaging.send(message)
+#         print('Successfully sent message:', response)
+
+#         Notification.objects.create(
+#             uid=user,
+#             title=instance.board.title,
+#             content=f'{instance.board.title} 새로운 댓글이 달렸어요!',
+#             board_id=instance.board,  # 수정된 부분
+#         )
+
         
 class mynotificationsviews(APIView):
     def get(self, request, uid, format=None):
