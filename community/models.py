62dc2c3369ae8412f7415ac89fa9021da346d1d8
ohatru <dusckd4948@naver.com>
2024-01-27 21:16:41 +0900

    1

--- community/models.py
+++ community/models.py
@@ -17,10 +17,12 @@ class Board(models.Model):
         return self.title
 
 class Comment(models.Model):
-    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="comments",null=True)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,null=True)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="comments", null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=100)
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"Comment on {self.board.title}"
+    def save(self, *args, **kwargs):
+        self.title = self.board.title
+        super(Comment, self).save(*args, **kwargs)
