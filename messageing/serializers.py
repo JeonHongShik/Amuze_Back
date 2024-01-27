f98b2d7b618808a5a7f0629862016d867f8056c2
ohatru <dusckd4948@naver.com>
2024-01-28 00:54:59 +0900

    1

--- messageing/serializers.py
+++ messageing/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from messageing.models import Notification
 
 class NotificationSerializer(serializers.ModelSerializer):
+    title = serializers.SerializerMethodField()
+    
+    
     class Meta:
         model = Notification
         fields = ['id','uid', 'title', 'content', 'board_id', 'created_at']
+
+    def get_title(self, obj):
+        return obj.title.title
