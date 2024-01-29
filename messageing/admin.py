caa2cb9e8093dcbe72f245b4dedbe863e39ef306
ohtaru <dusckd4948@naver.com>
2024-01-29 14:34:13 +0900

    1

--- messageing/admin.py
+++ messageing/admin.py
@@ -3,5 +3,5 @@ from .models import Notification
 
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
-    list_display = ['id','uid', 'title', 'content','board_id', 'created_at']
+    list_display = ['id','uid', 'title','messagebody','content','board_id', 'created_at']
     search_fields = ['title', 'content', 'author__username']
